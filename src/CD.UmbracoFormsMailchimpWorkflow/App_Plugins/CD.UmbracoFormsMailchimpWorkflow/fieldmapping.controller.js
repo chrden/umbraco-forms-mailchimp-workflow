fc44349bf92f964f1bb72f84d851229484614f92
Chris Dent <chrisdent14@gmail.com>
2021-04-16 23:46:37 +0100

    Added umbDropdown test

--- src/CD.UmbracoFormsMailchimpWorkflow/App_Plugins/CD.UmbracoFormsMailchimpWorkflow/fieldmapping.controller.js
+++ src/CD.UmbracoFormsMailchimpWorkflow/App_Plugins/CD.UmbracoFormsMailchimpWorkflow/fieldmapping.controller.js
@@ -1,10 +1,35 @@
 ﻿angular.module("umbraco").controller("CD.UmbracoFormsMailchimpWorkflow.FieldMapping",
     function ($scope, fieldmappingResource) {
 
-        $scope.getMcFields = function() {
-            fieldmappingResource.getMailchimpListMergeFields($scope.model.mcListId).then(function (response) {
-                $scope.mcFields = response.data
-            });
+        $scope.model.dropdownOpen = false;
+        $scope.model.items = [
+            { "name": "Item 1" },
+            { "name": "Item 2" },
+            { "name": "Item 3" }
+        ];
+
+        $scope.model.toggle = toggle;
+        $scope.model.close = close;
+        $scope.model.select = select;
+
+        function toggle() {
+            $scope.model.dropdownOpen = true;
+        }
+
+        function close() {
+            $scope.model.dropdownOpen = false;
+        }
+
+        function select(item) {
+            console.log(item);
+        }
+
+        $scope.getMcFields = function () {
+            $scope.model.dropdownOpen = true;
+
+            //fieldmappingResource.getMailchimpListMergeFields($scope.model.mcListId).then(function (response) {
+            //    $scope.mcFields = response.data
+            //});
         }
 
     });
