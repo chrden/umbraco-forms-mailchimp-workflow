e84626457c8ff740c69ebd092a8fd9ecce9c87b0
Chris Dent <chrisdent14@gmail.com>
2021-04-25 19:23:14 +0100

    Improved mapping fields with button group

--- src/CD.UmbracoFormsMailchimpWorkflow/App_Plugins/CD.UmbracoFormsMailchimpWorkflow/fieldmapping.controller.js
+++ src/CD.UmbracoFormsMailchimpWorkflow/App_Plugins/CD.UmbracoFormsMailchimpWorkflow/fieldmapping.controller.js
@@ -1,19 +1,57 @@
 ﻿angular.module("umbraco").controller("CD.UmbracoFormsMailchimpWorkflow.FieldMapping",
     function ($scope, fieldmappingResource) {
 
+        $scope.model.fieldMappings = [];
         $scope.model.showListsDropdown = false;
         $scope.model.showFieldsDropdown = false;
 
+        $scope.model.mapBtn = {
+            default: {
+                labelKey: "mapButton_defaultLabel",
+                handler: function () {
+                    $scope.addFieldMapping(false);
+                }
+            },
+            secondary: [
+                {
+                    labelKey: "mapButton_secondaryLabel",
+                    handler: function () {
+                        $scope.addFieldMapping(true);
+                    }
+                }
+            ]
+        }
+
         fieldmappingResource.getMailchimpLists().then(function (response) {
             $scope.model.showListsDropdown = true;
-            $scope.model.mcLists = response
+            $scope.model.lists = response
         });
 
         $scope.getListFields = function () {
-            fieldmappingResource.getMailchimpListMergeFields($scope.model.mcListId).then(function (response) {
-                $scope.model.showFieldsDropdown = true;
-                $scope.model.mcFields = response
-            });
+            if ($scope.model.listId) {
+                fieldmappingResource.getMailchimpListMergeFields($scope.model.listId).then(function (response) {
+                    $scope.model.showFieldsDropdown = true;
+                    $scope.model.listFields = response;
+                    $scope.model.fieldMappings = [];
+                    //$scope.addFieldMapping();
+                });
+            }
+            else {
+                $scope.model.fieldMappings = [];
+            }
         }
 
+        $scope.addFieldMapping = function (isStatic) {
+            $scope.model.fieldMappings.push({
+                formFieldAlias: "",
+                listFieldId: "",
+                staticValue: "",
+                isStatic: isStatic
+            });
+
+        };
+
+        $scope.deleteFieldMapping = function (i) {
+            $scope.model.fieldMappings.splice(i, 1);
+        };
     });
